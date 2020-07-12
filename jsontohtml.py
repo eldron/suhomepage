c105be2259db6c5214f6eb8c8c4d3388d5438815
eldron <eldron@163.com>
2020-07-12 17:11:09 +0800

    to test

---
+++ jsontohtml.py
@@ -0,0 +1,16 @@
+import json
+
+# <h4><FONT COLOR="#0080C0">-2020-</FONT><span style="font-size:15px;"></h4><ul>
+# <li> <b>Hybrid Routing: Towards Resilient Routing in Anonymous Communication Networks</b>. Yusheng Xia, Rongmao Chen, Jinshu Su, Chen Pan and Han Su. <FONT COLOR="#0080C0">IEEE ICC 2020</FONT> [<a href="./static/jinshusu.jpg"><FONT COLOR="#0080C0"> PDF </FONT> </a>]
+#  </li>
+
+file = open("papers.json","r")
+out = open("papers.html", "w")
+j = json.loads(file.read())
+file.close()
+
+for item in j["papers"]:
+	out.write("<h4><FONT COLOR=\"#0080C0\">-" + item["year"] + "-</FONT><span style=\"font-size:15px;\"></h4><ul>")
+	for paper in item["content"]:
+		out.write("<li> <b>" + paper["title"] + "</b>" + paper["authors"] + "<FONT COLOR=\"#0080C0\">" + paper["pubinfo"] + "</FONT> ")
+		out.write("[<a href=\"./static/" + paper["filename"] + "\"" + "><FONT COLOR=\"#0080C0\"> PDF </FONT> </a>]</li>")
